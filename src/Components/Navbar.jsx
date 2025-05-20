e06bb8bcfc37e88c7c4f39791154e239e7d05e79
whosnave <esardi10@gmail.com>
2025-05-20 23:45:27 +0700

    updd

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import Tutor from "../Assets/svgexport-4.svg";
 import Buku from "../Assets/svgexport-5.svg";
 import Discord from "../Assets/svgexport-6.svg";
 import Search from "../Assets/svgexport-7.svg";
-import Theme from "../Assets/svgexport-7.svg"; 
+import Theme from "../Assets/svgexport-7.svg";
 
 function Navbar() {
   return (
@@ -14,33 +14,40 @@ function Navbar() {
         <a href="">
           <img src={Logo} alt="Logo" className="h-8 ml-24" />
         </a>
-        <ul className="flex items-center gap-7 text-sm font-semibold">
+        <ul className="flex items-center ml-[-11px] gap-1 text-sm font-semibold">
           <li>
-            <a href="" className="flex items-center gap-1 hover:underline">
+            <a
+              href=""
+              className="flex items-center gap-1 px-3 py-2 rounded-xl hover:bg-white/15 transition duration-200"
+            >
               <img src={Home} alt="Home" className="h-5" />
-              <span className="text-[16px]">Home</span>
+              <span className="text-[16px] font-medium text-white">Home</span>
             </a>
           </li>
           <li>
-            <a href="" className="flex items-center gap-1 hover:underline">
+            <a href=""
+              className="flex items-center gap-1 px-3 py-2 rounded-xl hover:bg-white/15 transition duration-200">
               <img src={Kelas} alt="Kelas" className="h-6" />
               <span className="text-[16px]">Kelas</span>
             </a>
           </li>
           <li>
-            <a href="" className="flex items-center gap-1 hover:underline">
+            <a href=""
+              className="flex items-center gap-1 px-3 py-2 rounded-xl hover:bg-white/15 transition duration-200">
               <img src={Tutor} alt="Tutorial" className="h-5" />
               <span className="text-[16px]">Tutorial</span>
             </a>
           </li>
           <li>
-            <a href="" className="flex items-center gap-1 hover:underline">
+            <a href=""
+              className="flex items-center gap-1 px-3 py-2 rounded-xl hover:bg-white/15 transition duration-200">
               <img src={Buku} alt="Buku" className="h-5" />
               <span className="text-[16px]">Buku</span>
             </a>
           </li>
           <li>
-            <a href="" className="flex items-center gap-1 hover:underline">
+            <a href=""
+              className="flex items-center gap-1 px-3 py-2 rounded-xl hover:bg-white/15 transition duration-200">
               <img src={Discord} alt="Discord" className="h-4" />
               <span className="text-[16px]">Discord</span>
             </a>
@@ -50,10 +57,18 @@ function Navbar() {
 
       <div className="flex items-center gap-4">
         <button>
-          <img src={Search} alt="Search" className="h-5  opacity-80 hover:opacity-100 transition" />
+          <img
+            src={Search}
+            alt="Search"
+            className="h-5  opacity-80 hover:opacity-100 transition"
+          />
         </button>
         <button>
-          <img src={Theme} alt="Theme Toggle" className="h-5 opacity-80 hover:opacity-100 transition" />
+          <img
+            src={Theme}
+            alt="Theme Toggle"
+            className="h-5 opacity-80 hover:opacity-100 transition"
+          />
         </button>
         <a
           href="#"
